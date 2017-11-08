e063249c5af0fcd3de17d6a4987dac107f1ffcf7
HasanKhairullah <hassan.khairullah@gmail.com>
2017-11-08 15:07:11 +0700

    Complete all CRUD function for table City
    Create CRUD and adding GetByProvince in CityDataAccess

---
+++ BuahSayur/BuahSayur.ViewModel/CityViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuahSayur.ViewModel
+{
+    public class CityViewModel
+    {
+        public int Id { get; set; }
+
+        
+        public string Code { get; set; }
+
+        
+        public string Province_Code { get; set; }
+
+        public string ProvinceName { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string PostalCode { get; set; }
+        public DateTime? Created { get; set; }
+        [DisplayName("Created By")]
+        public string CreatedBy { get; set; }
+
+        public DateTime? Modified { get; set; }
+        [DisplayName("Modified By")]
+        public string ModifiedBy { get; set; }
+    }
+}
